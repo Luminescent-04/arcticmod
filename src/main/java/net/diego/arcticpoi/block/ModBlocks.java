d112184b28fd16c97c4a8664823c59fd148dce14
Luminescent-04 <deathrader0404@gmail.com>
2026-02-17 11:53:35 -0600

    Done

--- src/main/java/net/diego/arcticpoi/block/ModBlocks.java
+++ src/main/java/net/diego/arcticpoi/block/ModBlocks.java
@@ -3,6 +3,9 @@ package net.diego.arcticpoi.block;
 import net.diego.arcticpoi.ArcticPoi;
 import net.diego.arcticpoi.block.custom.RotationalBlock;
 import net.diego.arcticpoi.block.custom.RotationalNoCollideBlock;
+import net.diego.arcticpoi.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -11,52 +14,86 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.util.function.Supplier;
+
 public class ModBlocks {
 
-    // This creates a DeferredRegister for blocks.
-    // It tells Forge: "I will be registering blocks for my mod."
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, ArcticPoi.MOD_ID);
 
-    // RADIO BLOCK REGISTRATION
-    public static final RegistryObject<Block> RADIO = BLOCKS.register("radio",
-            () -> new RotationalNoCollideBlock(BlockBehaviour.Properties
-                    .of()                      // Basic block settings
-                    .mapColor(MapColor.METAL)  // Map color (used on maps)
-                    .strength(2.0f)            // Hardness (how long to break)
-                    .noOcclusion() // Doesn't block light / full cube rendering
-                    .noCollission()
-            ));
+    private static RegistryObject<Block> registerBlock(String name,
+                                                       Supplier<Block> blockSupplier) {
 
-    // CRATE BLOCK REGISTRATION
-    public static final RegistryObject<Block> CRATE = BLOCKS.register("crate",
-            () -> new Block(BlockBehaviour.Properties
-                    .of()                      // Basic block settings
-                    .strength(1.5F)            // Slightly weaker than stone
-                    .sound(SoundType.WOOD)     // Wood break/place sounds
-            ));
+        RegistryObject<Block> block = BLOCKS.register(name, blockSupplier);
 
-    public static final RegistryObject<Block> CHAIR = BLOCKS.register("chair",
-            () -> new RotationalBlock(BlockBehaviour.Properties
-                    .of()
-                    .strength(1.5F)
-                    .sound(SoundType.WOOD)
-                    .noOcclusion()
-            ));
+        ModItems.ITEMS.register(name,
+                () -> new BlockItem(block.get(), new Item.Properties()));
 
-    public static final RegistryObject<Block> WOODSTACK = BLOCKS.register("woodstack",
-            () -> new RotationalBlock(BlockBehaviour.Properties
-                    .of()
-                    .strength(1.5F)
-                    .sound(SoundType.WOOD)
-                    .noOcclusion()
-            ));
+        return block;
+    }
 
-    public static final RegistryObject<Block> BROKENCHAIR = BLOCKS.register("brokenchair",
-            () -> new RotationalNoCollideBlock(BlockBehaviour.Properties
-                    .of()
-                    .strength(1.5F)
-                    .sound(SoundType.WOOD)
-                    .noOcclusion()
-            ));
+    // RADIO
+    public static final RegistryObject<Block> RADIO =
+            registerBlock("radio",
+                    () -> new RotationalNoCollideBlock(BlockBehaviour.Properties
+                            .of()
+                            .mapColor(MapColor.METAL)
+                            .strength(2.0f)
+                            .noOcclusion()
+                            .noCollission()
+                    ));
+
+    // CRATE
+    public static final RegistryObject<Block> CRATE =
+            registerBlock("crate",
+                    () -> new Block(BlockBehaviour.Properties
+                            .of()
+                            .strength(1.5F)
+                            .sound(SoundType.WOOD)
+                    ));
+
+    public static final RegistryObject<Block> CHAIR =
+            registerBlock("chair",
+                    () -> new RotationalBlock(BlockBehaviour.Properties
+                            .of()
+                            .strength(1.5F)
+                            .sound(SoundType.WOOD)
+                            .noOcclusion()
+                    ));
+
+    public static final RegistryObject<Block> WOODSTACK =
+            registerBlock("woodstack",
+                    () -> new RotationalBlock(BlockBehaviour.Properties
+                            .of()
+                            .strength(1.5F)
+                            .sound(SoundType.WOOD)
+                            .noOcclusion()
+                    ));
+
+    public static final RegistryObject<Block> BROKENCHAIR =
+            registerBlock("brokenchair",
+                    () -> new RotationalNoCollideBlock(BlockBehaviour.Properties
+                            .of()
+                            .strength(1.5F)
+                            .sound(SoundType.WOOD)
+                            .noOcclusion()
+                    ));
+
+    public static final RegistryObject<Block> SHELF =
+            registerBlock("shelf",
+                    () -> new RotationalBlock(BlockBehaviour.Properties
+                            .of()
+                            .strength(1.5F)
+                            .sound(SoundType.WOOD)
+                            .noOcclusion()
+                    ));
+
+    public static final RegistryObject<Block> TABLE =
+            registerBlock("table",
+                    () -> new Block(BlockBehaviour.Properties
+                            .of()
+                            .strength(1.5F)
+                            .sound(SoundType.WOOD)
+                            .noOcclusion()
+                    ));
 }
--- src/main/java/net/diego/arcticpoi/item/ModItems.java
+++ src/main/java/net/diego/arcticpoi/item/ModItems.java
@@ -1,41 +1,13 @@
 package net.diego.arcticpoi.item;
 
 import net.diego.arcticpoi.ArcticPoi;
-import net.diego.arcticpoi.block.ModBlocks;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 public class ModItems {
 
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, ArcticPoi.MOD_ID);
-
-    public static final RegistryObject<Item> RADIO =
-            ITEMS.register("radio",
-                    () -> new BlockItem(ModBlocks.RADIO.get(),
-                            new Item.Properties()));
-
-    public static final RegistryObject<Item> CRATE =
-            ITEMS.register("crate",
-            () -> new BlockItem(ModBlocks.CRATE.get(),
-                    new Item.Properties()));
-
-    public static final RegistryObject<Item> CHAIR =
-            ITEMS.register("chair",
-                    () -> new BlockItem(ModBlocks.CHAIR.get(),
-                            new Item.Properties()));
-
-    public static final RegistryObject<Item> WOODSTACK =
-            ITEMS.register("woodstack",
-                    () -> new BlockItem(ModBlocks.WOODSTACK.get(),
-                            new Item.Properties()));
-
-    public static final RegistryObject<Item> BROKENCHAIR =
-            ITEMS.register("brokenchair",
-                    () -> new BlockItem(ModBlocks.BROKENCHAIR.get(),
-                            new Item.Properties()));
 
 }
